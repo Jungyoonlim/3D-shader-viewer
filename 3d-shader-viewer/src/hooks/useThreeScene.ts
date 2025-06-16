896f4c66780550df1f9501b4781373621e61e919
Jungyoon Lim <jungyoonlim@users.noreply.github.com>
2025-06-16 11:36:35 +0900

    commit

---
+++ 3d-shader-viewer/src/hooks/useThreeScene.ts
@@ -0,0 +1,5 @@
+import { useEffect, useCallback } from 'react';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { useSceneStore } from '../stores/sceneStore';
+
