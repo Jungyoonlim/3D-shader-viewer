f38604870432553c3043d899fd9b791e0e3e681a
Jungyoon Lim <jungyoonlim@users.noreply.github.com>
2025-06-16 12:25:37 +0900

    use Animation hook

--- 3d-shader-viewer/src/hooks/useAnimations.ts
+++ 3d-shader-viewer/src/hooks/useAnimations.ts
@@ -3,5 +3,21 @@ import { useSceneStore } from '@/stores/sceneStore';
 
 export const useAnimation = () => {
     const animationFrameRef = useRef<number>();
+    const lastTimeRef = useRef<number>(0);
+    const fpsRef = useRef<number[]>([]);
+    const frameCountRef = useRef<number>(0);
+
+    const { 
+        scene,
+        camera,
+        renderer,
+        controls,
+        updatePerformance
+    } = useSceneStore();
+
+    const updatePerformanceStats = useCallback((deltatime: number) => {
+        
+    })
+
     
 }
